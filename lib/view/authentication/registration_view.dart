6b03c8b2ecd93c48c1f91070ce491e6e3131ca9c
Andrea Gonzales <AndreaGon@users.noreply.github.com>
2023-08-11 23:16:47 +0800

    Basic ui for home page

--- lib/view/authentication/registration_view.dart
+++ lib/view/authentication/registration_view.dart
@@ -1,62 +1,130 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_month_picker/flutter_custom_month_picker.dart';
+import 'package:intl/intl.dart';
 import 'package:shuttle_stalk/res/colors.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shuttle_stalk/view/authentication/login_view.dart';
 import 'package:shuttle_stalk/view_model/authentication_view_model.dart';
-class Registration extends StatelessWidget {
+
+class Registration extends StatefulWidget {
+  @override
+  State<Registration> createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
   final formKey = GlobalKey<FormState>();
+
   final username = TextEditingController();
+
   final email = TextEditingController();
+
   final password = TextEditingController();
+
+  String graduation_month = "Graduation Month and Year";
+
+  String graduation_year = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: darkblue,
         appBar: null,
-        body: new Container(
-            margin: new EdgeInsets.all(15.0),
-            child: new Form(
+        body: Container(
+            margin: const EdgeInsets.all(15.0),
+            child: Form(
               key: formKey,
-              child: new Column(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  new Text("Register an Account",
+                  const Text("Register an Account",
                       style: TextStyle(height: 5, fontSize: 25, color: white)
                   ),
-                  new Container(
+                  Container(
                     margin: const EdgeInsets.only(top: 20.0, bottom: 10.0),
-                    child: new TextFormField(
+                    child: TextFormField(
                         obscureText: false,
                         controller: username,
-                        decoration: InputDecoration(
+                        decoration: const InputDecoration(
                             border: OutlineInputBorder(),
-                            labelText: 'Username',
-                            filled: true, //<-- SEE HERE
+                            labelText: 'Full Name',
+                            filled: true,
                             fillColor: white
                         )
                     ),
                   ),
-                  new Container(
+                  Container(
                     margin: const EdgeInsets.only(bottom: 10.0),
-                    child: new TextFormField(
+                    child: TextFormField(
+                        obscureText: false,
+                        controller: username,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Program (e.g. BCSCUN)',
+                            filled: true,
+                            fillColor: white
+                        )
+                    ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(bottom: 10.0),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50.0,
+                        child: ElevatedButton(
+                          child: Text(graduation_month + " " + graduation_year, style: TextStyle(fontWeight: FontWeight.normal, color: Colors.black),textAlign: TextAlign.left,),
+                          style: ElevatedButton.styleFrom(
+                            alignment: Alignment.centerLeft,
+                            primary: white,
+                            elevation: 0,
+                          ),
+                          onPressed: () {
+                            showMonthPicker(context, onSelected: (month, year) {
+                              if (kDebugMode) {
+                                print('Selected month: $month, year: $year');
+                              }
+
+                              setState(() {
+                                this.graduation_month = DateFormat('MMMM').format(DateTime(0, month));
+                                this.graduation_year = DateFormat('yyyy').format(DateTime(year));
+                              });
+                            },
+                            firstEnabledMonth: 3,
+                            lastEnabledMonth: 10,
+                            firstYear: 2000,
+                            lastYear: 3000,
+                            selectButtonText: 'OK',
+                            cancelButtonText: 'Cancel',
+                            highlightColor: darkblue,
+                            textColor: Colors.black,
+                            contentBackgroundColor: Colors.white,
+                            dialogBackgroundColor: Colors.grey[200]);
+                          },
+                        ),
+                      )
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10.0),
+                    child: TextFormField(
                       obscureText: false,
                       controller: email,
-                      decoration: InputDecoration(
+                      decoration: const InputDecoration(
                           border: OutlineInputBorder(),
-                          labelText: 'Email',
+                          labelText: 'Student Email',
                           filled: true,
                           fillColor: white
                       ),
                     ),
                   ),
-                  new Container(
+                  Container(
                     margin: const EdgeInsets.only(bottom: 10.0),
-                    child: new TextFormField(
+                    child: TextFormField(
                       obscureText: true,
                       controller: password,
-                      decoration: InputDecoration(
+                      decoration: const InputDecoration(
                           border: OutlineInputBorder(),
                           labelText: 'Password',
                           filled: true,
@@ -64,8 +132,7 @@ class Registration extends StatelessWidget {
                       ),
                     ),
                   ),
-                  new ElevatedButton(
-                      child: new Text("Register"),
+                  ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         primary: lightblue,
                         minimumSize: Size.fromHeight(40), // fromHeight use double.infinity as width and 40 is the height
@@ -74,16 +141,16 @@ class Registration extends StatelessWidget {
                         if(formKey.currentState!.validate()){
                           Authentication().registerUser(context, username.text.trim(), email.text.trim(), password.text.trim());
                         }
-                      }
+                      },
+                      child: const Text("Register")
                   ),
-                  new Container(
+                  Container(
                     margin: const EdgeInsets.only(top: 20.0, bottom: 20.0),
-                    child: new Text("- Already have an account? -",
+                    child: const Text("- Already have an account? -",
                         style: TextStyle(fontSize: 15, color: white)
                     ),
                   ),
-                  new ElevatedButton(
-                      child: new Text("Login"),
+                  ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         primary: lightblue,
                         minimumSize: Size.fromHeight(40), // fromHeight use double.infinity as width and 40 is the height
@@ -94,7 +161,8 @@ class Registration extends StatelessWidget {
                             builder: (context) => Login(),
                           ),
                         );
-                      }
+                      },
+                      child: const Text("Login")
                   )
                 ],
               )
--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shuttle_stalk/res/colors.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,8 +12,71 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("My Home Page"),
+    return Scaffold(
+        appBar: null,
+        backgroundColor: lightblue,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+                crossAxisAlignment : CrossAxisAlignment.start,
+                children : [
+                  Column(
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.only(top: 70.0, left: 40.0),
+                          child: Container(
+                            child: Text("Welcome,", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),),
+                          )
+                      ),
+                      Padding(
+                          padding: EdgeInsets.only(top: 10.0, left: 10.0),
+                          child: Container(
+                            child: Text("Andrea!", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),),
+                          )
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 40.0, left: 60.0),
+                    child: Icon(
+                      Icons.emoji_transportation,
+                      color: Colors.white,
+                      size: 120.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    ),
+                  )
+                ]
+            ),
+            // Padding(
+            //     padding: EdgeInsets.only(top: 10.0, left: 30.0),
+            //     child: Container(
+            //       child: Text("Andrea!", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),),
+            //     )
+            // ),
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: Padding(
+                    padding: EdgeInsets.only(bottom: 0.0),
+                    child: Container(
+                        height: 530,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(70),
+                              topRight: Radius.circular(70),
+                            )
+                        ),
+                        child: Text("")
+                    )//Your widget here,
+                ),
+              ),
+            ),
+          ],
+        )
     );
   }
 }
